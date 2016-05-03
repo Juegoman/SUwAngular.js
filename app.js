aa5812aca3a16e920b52e03f39c20c67467b13e4
Ruben Gallegos <gallegos_ruben@yahoo.com>
2016-05-02 23:56:20 -0700

    2.3 -> 4.1

--- app.js
+++ app.js
@@ -5,20 +5,65 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller('ReviewController', function(){
+    this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
+  app.directive('productTitle', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-title.html'
+    };
+  });
+
   var gems = [
     {
       name: 'Dodecahedron',
       price: 2.95,
-      description: 'It\'s got 12 sides, so there are some applications',
+      description: 'It\'s got 12 sides, so there are some applications.',
       canPurchase: true,
-      image: "images/gem1.jpg"
+      image: "images/gem1.jpg",
+      reviews: [
+        {
+          stars: 3,
+          body: "it's ok, I guess",
+          author: "kek@lord.lel"
+        },
+      ]
     },
     {
       name: 'Pentagonal Gem',
       price: 5.95,
       description: 'One more angle than a square gem.',
       canPurchase: false,
-      image:"images/gem2.jpg"
+      image:"images/gem2.jpg",
+      reviews: [
+        {
+          stars: 4,
+          body: "Met my expectations, although it could use another side.",
+          author: "wew@top.pip"
+        },
+        {
+          stars: 1,
+          body: "dumb",
+          author: "anonymouse"
+        }
+      ]
     }
-  ]
+  ];
 })();
