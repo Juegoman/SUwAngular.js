2652907fdff32c61c7d48d66665c1d5f74bb5b95
Ruben Gallegos <gallegos_ruben@yahoo.com>
2016-05-02 19:34:24 -0700

    1.1 -> 2.3

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-var app = angular.module('store', [ ]);
+(function() {
+  var app = angular.module('store', [ ]);
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'It\'s got 12 sides, so there are some applications',
+      canPurchase: true,
+      image: "images/gem1.jpg"
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: 'One more angle than a square gem.',
+      canPurchase: false,
+      image:"images/gem2.jpg"
+    }
+  ]
+})();
